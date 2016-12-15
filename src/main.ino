a2434a809933257030790dcfe48691d64603282b
Jamie Sanson <jamie.h.sanson@gmail.com>
2016-12-15 21:28:45 +1300

    Begins work on base sketch

---
+++ src/main.ino
@@ -0,0 +1,27 @@
+/*
+----------------------------------------------------------------------------------------
+                            Macro mechanical keyboard main sketch
+----------------------------------------------------------------------------------------
+
+    Contains code relating to the operation of a Teensy LC board connected to 6 Cherry
+    MX compatible switches. To use, redefine the constants used for active-high button
+    input pins.
+
+*/
+
+// region pin definitions
+#define button1 2
+#define button2 -1
+#define button3 -1
+#define button4 -1
+#define button5 -1
+#define button6 -1
+// end region
+
+void setup() {
+    
+}
+
+void loop() {
+
+}
